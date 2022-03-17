f5ea850ec209a4f46dfe4d9cae1c970ad18e3840
Cainã Milech <99932600+cainamilech@users.noreply.github.com>
2022-03-17 10:05:36 -0300

    Add files via upload

---
+++ processa_envio.php
@@ -0,0 +1,7 @@
+<?php 
+
+	//isso é uma camada de segurança, para tirar o arquivo original com todas informações do diretorio publico htdocs, entao foi criado fora e feito o require.
+
+	require "../../app_send_mail/processa_envio.php";
+
+?>
